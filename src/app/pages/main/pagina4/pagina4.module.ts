dffa296565c1e2c46984c4ce349f4b37a73b8df9
dianahndz <140652793+dianahndz@users.noreply.github.com>
2024-05-08 13:18:55 -0600

    segundo cambio

--- src/app/pages/main/pagina4/pagina4.module.ts
+++ src/app/pages/main/pagina4/pagina4.module.ts
@@ -9,6 +9,7 @@ import { Pagina4PageRoutingModule } from './pagina4-routing.module';
 import { Pagina4Page } from './pagina4.page';
 import { SharedModule } from 'src/app/shared/shared.module';
 
+
 @NgModule({
   imports: [
     CommonModule,
